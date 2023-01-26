1280f2b735ec54f4d3b7a6320467800fb9400320
DICE_shenyu <jisuanji888888@outlook.com>
2023-01-26 11:15:48 +0800

    Update

--- normalGacha.py
+++ normalGacha.py
@@ -8,6 +8,15 @@ with open('./gacha_table.json', 'r', encoding='utf-8') as f:
 with open('./character_table.json', 'r', encoding='utf-8') as f:
     CHARACTER_TABLE = json.load(f)
 
+player_data = {
+    "track": {
+        "recruit": {
+            "pool": None
+        }
+    }
+}
+tag_list = []
+
 
 def parse_recruitable_chars(s: str) -> Set[str]:
 
@@ -83,8 +92,8 @@ def refresh_tag_list() -> List:
         "6star": 0.210417,
         "5star": 0.523127,
         "4star": 14.988323,
-        "3star": 81.11354,
-        "2star": 1.51041,
+        "3star": 79.11354,
+        "2star": 3.51041,
         "1star": 0.554183
     }
     tags_set = []
@@ -93,12 +102,13 @@ def refresh_tag_list() -> List:
     ranks, probs = zip(*rank_weights.items())
 
     while len(tags_set) < 5:
-        random_group = random.choices(ranks, probs, k=4)
+        random_group = random.choices(ranks, probs, k=10)
         char_pool = [random.choice(chars_list[int(group[:1]) - 1]) for group in random_group]
         tags_set = list(set([tag for char in char_pool for tag in char_data[char]["tags"]]))
 
     tag_list = random.sample(tags_set, 5)
     tag_list.sort()
+    player_data["track"]["recruit"]["pool"] = char_pool
 
     tag2name = {v["tagId"]: v["tagName"] for v in GACHA_TABLE["gachaTags"][:-2]}
     for i in tag_list:
@@ -107,4 +117,50 @@ def refresh_tag_list() -> List:
     return tag_list
 
 
+def generate_valid_tags(duration: int) -> tuple[str, List]:
+
+    char_list, char_data = generate_recruitable_data()
+    selected_tags = random.sample(tag_list, 3)
+    print(f"selected_tags：\t\t{selected_tags}")
+    if duration <= 13800:
+        char_range = [0, 3]
+    elif duration <= 27000:
+        char_range = [1, 4]
+    else:
+        if 11 in selected_tags:
+            char_range = [4, 4]
+        elif 14 in selected_tags:
+            char_range = [5, 5]
+        else:
+            char_range = [2, 4]
+
+    char_pool = player_data["track"]["recruit"]["pool"]
+
+    alternate_list = []
+    for charId in char_pool:
+        if char_range[0] <= char_data[charId]["rarity"] <= char_range[1]:
+            alternate_list.append(charId)
+
+    alternate_char_data = {k: v for k, v in char_data.items() if k in alternate_list}
+    matching_chars = {char: alternate_char_data[char] for char in alternate_char_data if set(alternate_char_data[char]['tags']).intersection(set(selected_tags))}
+    sorted_matching_chars = sorted(matching_chars.items(), key=lambda x: len(set(x[1]['tags']).intersection(set(selected_tags))), reverse=True)
+    print(f"matching_chars：\t{sorted_matching_chars}")
+    if len(sorted_matching_chars) == 0:
+        char_range[1] += 1
+        group_weights = [5 if rank == 0 else 15 if rank == 1 else 77 if rank == 2 else 2 if rank == 3 else 1 for rank in range(*char_range)]
+        group = random.choices([rank for rank in range(*char_range)], group_weights, k=1)[0]
+        all_chars = [char for char in char_list[group]]
+        random_char_id = random.choice(all_chars)
+    else:
+        random_char_id = random.choice(sorted_matching_chars)[0]
+
+    filter_tags = [x for x in selected_tags if x not in char_data[random_char_id]["tags"]]
+    print(f"random_char_id：\t{random_char_id}\nfilter_tags：\t\t{filter_tags}")
+
+    return random_char_id, filter_tags
+
+
 tag_list = refresh_tag_list()
+print(f"tag_list：\t\t{tag_list}")
+
+generate_valid_tags(27000)
